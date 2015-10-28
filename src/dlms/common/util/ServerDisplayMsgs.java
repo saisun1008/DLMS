251ff023914655b6f681d9b13eb3cb5c841738f8
saisun1008 <saisun1008@gmail.com>
2015-10-28 09:24:25 -0400

    This is a working version, need to work on transfer loan now

--- src/dlms/common/util/ServerDisplayMsgs.java
+++ src/dlms/common/util/ServerDisplayMsgs.java
@@ -5,176 +5,201 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
- * Utility class to print out messages in the console
- * Created by Sai on 2015/5/25.
+ * Utility class to print out messages in the console Created by Sai on
+ * 2015/5/25.
  */
 public class ServerDisplayMsgs
 {
-	private final static String WELCOME = "Welcome to the DLMS service!\n";
-	private final static String SEPARATOR = "*************************************************\n*************************************************";
-	private final static String CUSTOMER_OPERATIONS = "1.Open Account\n2.Get Loan\n3.Exit";
-	private final static String MANAGER_OPERATIONS = "1.Delay loan due date\n2.Print customer info\n3.Exit";
-	private final static String SELECTION = "Please enter selection:";
-	private static InputStream m_inputStream = null;
+    private final static String WELCOME = "Welcome to the DLMS service!\n";
+    private final static String SEPARATOR = "*************************************************\n*************************************************";
+    private final static String CUSTOMER_OPERATIONS = "1.Open Account\n2.Get Loan\n3. Transfer loan\n";
+    private final static String MANAGER_OPERATIONS = "4.Delay loan due date\n5.Print customer info\n6.Exit";
+    private final static String SELECTION = "Please enter selection:";
+    private static InputStream m_inputStream = null;
 
-	public static void printWelcome()
-	{
-		System.out.println(SEPARATOR);
-		System.out.println(WELCOME);
-		System.out.println(SEPARATOR);
-	}
+    public static void printWelcome()
+    {
+        System.out.println(SEPARATOR);
+        System.out.println(WELCOME);
+        System.out.println(SEPARATOR);
+    }
 
-	public static int printCustomerOps()
-	{
-		System.out.println(SEPARATOR);
-		System.out.println(CUSTOMER_OPERATIONS);
-		System.out.println(SEPARATOR);
-		System.out.println(SELECTION);
-		if (m_inputStream == null)
-		{
-			m_inputStream = System.in;
-		}
-		Scanner scan = new Scanner(m_inputStream);
-		int option = scan.nextInt();
-		return option;
-	}
+    public static int printCustomerOps()
+    {
+        System.out.println(SEPARATOR);
+        System.out.println(CUSTOMER_OPERATIONS);
+        System.out
+                .println("Following two operations are for managers, login will be needed");
+        System.out.println(MANAGER_OPERATIONS);
+        System.out.println(SEPARATOR);
+        System.out.println(SELECTION);
+        if (m_inputStream == null)
+        {
+            m_inputStream = System.in;
+        }
+        Scanner scan = new Scanner(m_inputStream);
+        int option = scan.nextInt();
+        return option;
+    }
 
-	public static int printManagerOps()
-	{
-		System.out.println(SEPARATOR);
-		System.out.println(MANAGER_OPERATIONS);
-		System.out.println(SEPARATOR);
-		System.out.println(SELECTION);
-		if (m_inputStream == null)
-		{
-			m_inputStream = System.in;
-		}
-		Scanner scan = new Scanner(m_inputStream);
-		int option = scan.nextInt();
-		return option;
-	}
+    public static int printManagerOps()
+    {
+        System.out.println(SEPARATOR);
+        System.out.println(MANAGER_OPERATIONS);
+        System.out.println(SEPARATOR);
+        System.out.println(SELECTION);
+        if (m_inputStream == null)
+        {
+            m_inputStream = System.in;
+        }
+        Scanner scan = new Scanner(m_inputStream);
+        int option = scan.nextInt();
+        return option;
+    }
 
-	public static void printResult(boolean ret)
-	{
-		System.out.println(SEPARATOR);
-		if (ret)
-		{
-			System.out.println("Operation successful");
-		} else
-		{
-			System.out.println("Operation failed");
-		}
-		System.out.println(SEPARATOR);
-	}
+    public static void printResult(boolean ret)
+    {
+        System.out.println(SEPARATOR);
+        if (ret)
+        {
+            System.out.println("Operation successful");
+        } else
+        {
+            System.out.println("Operation failed");
+        }
+        System.out.println(SEPARATOR);
+    }
 
-	public static void flushConsole()
-	{
-		System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n");
-	}
+    public static void flushConsole()
+    {
+        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n");
+    }
 
-	public static void printList(String title, ArrayList<String> list)
-	{
-		System.out.println(SEPARATOR);
-		System.out.println(title);
-		for (String item : list)
-		{
-			System.out.println((list.indexOf(item) + 1) + ". " + item);
-		}
-		System.out.println(SEPARATOR);
-	}
+    public static void printList(String title, ArrayList<String> list)
+    {
+        System.out.println(SEPARATOR);
+        System.out.println(title);
+        for (String item : list)
+        {
+            System.out.println((list.indexOf(item) + 1) + ". " + item);
+        }
+        System.out.println(SEPARATOR);
+    }
 
-	public static String getCustomerInfo()
-	{
-		if (m_inputStream == null)
-		{
-			m_inputStream = System.in;
-		}
-		Scanner scan = new Scanner(m_inputStream);
-		String info = "";
-		System.out.println(SEPARATOR);
-		System.out.println("Please enter customer first name: ");
-		info += scan.nextLine();
-		info += ";;d";
-		System.out.println("Please enter customer last name: ");
-		info += scan.nextLine();
-		info += ";;d";
-		System.out.println("Please enter email address: ");
-		info += scan.nextLine();
-		info += ";;d";
-		System.out.println("Please enter phone number: ");
-		info += scan.nextLine();
-		info += ";;d";
-		System.out.println("Please enter password: ");
-		info += scan.nextLine();
+    public static String getCustomerInfo()
+    {
+        if (m_inputStream == null)
+        {
+            m_inputStream = System.in;
+        }
+        Scanner scan = new Scanner(m_inputStream);
+        String info = "";
+        System.out.println(SEPARATOR);
+        System.out.println("Please enter customer first name: ");
+        info += scan.nextLine();
+        info += ";;d";
+        System.out.println("Please enter customer last name: ");
+        info += scan.nextLine();
+        info += ";;d";
+        System.out.println("Please enter email address: ");
+        info += scan.nextLine();
+        info += ";;d";
+        System.out.println("Please enter phone number: ");
+        info += scan.nextLine();
+        info += ";;d";
+        System.out.println("Please enter password: ");
+        info += scan.nextLine();
 
-		System.out.println(SEPARATOR);
-		return info;
-	}
+        System.out.println(SEPARATOR);
+        return info;
+    }
 
-	public static String applyLoan()
-	{
-		if (m_inputStream == null)
-		{
-			m_inputStream = System.in;
-		}
-		Scanner scan = new Scanner(m_inputStream);
-		String info = "";
-		System.out.println(SEPARATOR);
-		System.out.println("Please enter account number: ");
-		info += scan.nextLine();
-		info += ";;d";
-		System.out.println("Please enter password: ");
-		info += scan.nextLine();
-		info += ";;d";
-		System.out.println("Please enter loan amount: ");
-		info += scan.nextLine();
+    public static String applyLoan()
+    {
+        if (m_inputStream == null)
+        {
+            m_inputStream = System.in;
+        }
+        Scanner scan = new Scanner(m_inputStream);
+        String info = "";
+        System.out.println(SEPARATOR);
+        System.out.println("Please enter account number: ");
+        info += scan.nextLine();
+        info += ";;d";
+        System.out.println("Please enter password: ");
+        info += scan.nextLine();
+        info += ";;d";
+        System.out.println("Please enter loan amount: ");
+        info += scan.nextLine();
 
-		System.out.println(SEPARATOR);
-		return info;
-	}
+        System.out.println(SEPARATOR);
+        return info;
+    }
 
-	public static String delayLoan()
-	{
-		if (m_inputStream == null)
-		{
-			m_inputStream = System.in;
-		}
-		Scanner scan = new Scanner(m_inputStream);
-		String info = "";
-		System.out.println(SEPARATOR);
-		System.out.println("Please enter loan id: ");
-		info += scan.nextLine();
-		info += ";;d";
-		System.out.println("Please current due date(mm/dd/yyyy): ");
-		info += scan.nextLine();
-		info += ";;d";
-		System.out.println("Please enter new due date(mm/dd/yyyy): ");
-		info += scan.nextLine();
+    public static String transferLoan()
+    {
+        if (m_inputStream == null)
+        {
+            m_inputStream = System.in;
+        }
+        Scanner scan = new Scanner(m_inputStream);
+        String info = "";
+        System.out.println(SEPARATOR);
+        System.out.println("Please enter loan id: ");
+        info += scan.nextLine();
+        info += ";;d";
+        System.out.println("Please enter current bank: ");
+        info += scan.nextLine();
+        info += ";;d";
+        System.out.println("Please enter bank to be transfered to: ");
+        info += scan.nextLine();
 
-		System.out.println(SEPARATOR);
-		return info;
-	}
-	
-	public static String login()
-	{
-		if (m_inputStream == null)
-		{
-			m_inputStream = System.in;
-		}
-		Scanner scan = new Scanner(m_inputStream);
-		String info = "";
-		System.out.println(SEPARATOR);
-		System.out.println("Please enter user name or account id: ");
-		info += scan.nextLine();
-		info += ";;d";
-		System.out.println("Please enter password: ");
-		info += scan.nextLine();
-		System.out.println(SEPARATOR);
-		return info;
-	}
+        System.out.println(SEPARATOR);
+        return info;
+    }
 
-	public static void setInputStream(InputStream inputStream)
-	{
-		m_inputStream = inputStream;
-	}
+    public static String delayLoan()
+    {
+        if (m_inputStream == null)
+        {
+            m_inputStream = System.in;
+        }
+        Scanner scan = new Scanner(m_inputStream);
+        String info = "";
+        System.out.println(SEPARATOR);
+        System.out.println("Please enter loan id: ");
+        info += scan.nextLine();
+        info += ";;d";
+        System.out.println("Please current due date(mm/dd/yyyy): ");
+        info += scan.nextLine();
+        info += ";;d";
+        System.out.println("Please enter new due date(mm/dd/yyyy): ");
+        info += scan.nextLine();
+
+        System.out.println(SEPARATOR);
+        return info;
+    }
+
+    public static String login()
+    {
+        if (m_inputStream == null)
+        {
+            m_inputStream = System.in;
+        }
+        Scanner scan = new Scanner(m_inputStream);
+        String info = "";
+        System.out.println(SEPARATOR);
+        System.out.println("Please enter user name or account id: ");
+        info += scan.nextLine();
+        info += ";;d";
+        System.out.println("Please enter password: ");
+        info += scan.nextLine();
+        System.out.println(SEPARATOR);
+        return info;
+    }
+
+    public static void setInputStream(InputStream inputStream)
+    {
+        m_inputStream = inputStream;
+    }
 }
--- src/dlms/controller/corba/CorbaClientController.java
+++ src/dlms/controller/corba/CorbaClientController.java
@@ -1,6 +1,171 @@
 package dlms.controller.corba;
 
+import java.util.Properties;
+import java.util.Scanner;
+
+import org.omg.CORBA.ORB;
+import org.omg.CORBA.ORBPackage.InvalidName;
+import org.omg.CosNaming.NameComponent;
+import org.omg.CosNaming.NamingContextExt;
+import org.omg.CosNaming.NamingContextExtHelper;
+import org.omg.CosNaming.NamingContextPackage.CannotProceed;
+import org.omg.CosNaming.NamingContextPackage.NotFound;
+import org.omg.PortableServer.POA;
+import org.omg.PortableServer.POAHelper;
+import org.omg.PortableServer.POAManagerPackage.AdapterInactive;
+
+import dlms.common.Configuration;
+import dlms.common.util.ServerDisplayMsgs;
+import dlms.interfaces.corba.DlmsInterfaceModule.DlmsInterface;
+import dlms.interfaces.corba.DlmsInterfaceModule.DlmsInterfaceHelper;
+
 public class CorbaClientController
 {
+    /**
+     * Constructor
+     * 
+     * @param port
+     */
+    public CorbaClientController()
+    {
+    }
 
+    public static void main(String[] args)
+    {
+        boolean terminate = false;
+        while (!terminate)
+        {
+            DlmsInterface service = null;
+            String bank;
+            ServerDisplayMsgs.printWelcome();
+            Properties props = System.getProperties();
+            props.put("org.omg.CORBA.ORBInitialPort",
+                    Integer.toString(Configuration.CORBA_NAMING_SERVICE_PORT));
+            props.put("org.omg.CORBA.ORBInitialHost",
+                    Configuration.CORBA_NAMING_SERVICE_HOST);
+            ORB orb = ORB.init(args, null);
+
+            // get the root naming context
+            org.omg.CORBA.Object objRef = null;
+            try
+            {
+                objRef = orb.resolve_initial_references("NameService");
+            } catch (InvalidName e1)
+            {
+                e1.printStackTrace();
+            }
+            // Use NamingContextExt instead of NamingContext. This is
+            // part of the Interoperable naming Service.
+            NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
+
+            System.out.println("List of available services:");
+            for (int i = 0; i < Configuration.BANK_NAME_POOL.length; i++)
+            {
+                System.out.println(Configuration.BANK_NAME_POOL[i]);
+            }
+
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Please enter desired bank to proceed");
+            bank = scan.nextLine();
+            try
+            {
+                if (service != null)
+                    service._release();
+                service = DlmsInterfaceHelper.narrow(ncRef.resolve_str(bank));
+
+                POA rootPOA = POAHelper.narrow(orb
+                        .resolve_initial_references("RootPOA"));
+                // Resolve MessageServer
+                NameComponent[] nc =
+                { new NameComponent("MessageServer", "") };
+                rootPOA.the_POAManager().activate();
+            } catch (NotFound | CannotProceed
+                    | org.omg.CosNaming.NamingContextPackage.InvalidName e)
+            {
+                e.printStackTrace();
+            } catch (AdapterInactive e)
+            {
+                e.printStackTrace();
+            } catch (InvalidName e)
+            {
+                e.printStackTrace();
+            }
+            CorbaClientController controller = new CorbaClientController();
+            switch (ServerDisplayMsgs.printCustomerOps())
+            {
+            case 1:
+                String[] info = ServerDisplayMsgs.getCustomerInfo()
+                        .split(";;d");
+                String ret = service.openAccount(bank, info[0], info[1],
+                        info[2], info[3], info[4]);
+                if (ret != null)
+                {
+                    System.out
+                            .println("Account has been created, account id is "
+                                    + ret);
+                } else
+                {
+                    System.out.println("Failed to open account ");
+                }
+                break;
+            case 2:
+                String[] info1 = ServerDisplayMsgs.applyLoan().split(";;d");
+                String result = service.getLoan(bank, info1[0], info1[1],
+                        Double.parseDouble(info1[2]));
+
+                if (result != null)
+                {
+                    System.out.println("Loan has been created, loan id is "
+                            + result);
+                } else
+                {
+                    System.out.println("Failed to get a loan ");
+                }
+                break;
+            case 3:
+                String[] info2 = ServerDisplayMsgs.transferLoan().split(";;d");
+                String reply = service.transferLoan(info2[0], info2[1],
+                        info2[2]);
+
+                if (reply != null)
+                {
+                    System.out.println("Loan has been transfered, loan id is "
+                            + reply);
+                } else
+                {
+                    System.out.println("Failed to transfer a loan ");
+                }
+                break;
+            case 4:
+                String[] info3 = ServerDisplayMsgs.delayLoan().split(";;d");
+                boolean ret1 = service.delayPayment(bank, info3[0], info3[1],
+                        info3[2]);
+                if (ret1)
+                {
+                    System.out.println("Loan has been successfully delayed "
+                            + ret1);
+                } else
+                {
+                    System.out.println("Failed to delay the loan ");
+                }
+                break;
+            case 5:
+                String result1 = service.printCustomerInfo(bank);
+                for (String str : result1.split("\n"))
+                {
+                    System.out.println(str);
+                }
+                break;
+            case 6:
+                terminate = true;
+                break;
+            default:
+                terminate = true;
+                break;
+            }
+            System.out.println("Press enter key to continue...");
+            scan.nextLine();
+            ServerDisplayMsgs.flushConsole();
+        }
+    }
 }
