e4ee79c9b9c7a659d0df3062e04b776781077928
saisun1008 <saisun1008@gmail.com>
2015-11-21 19:36:02 -0500

    better version

--- src/dlms/service/web/client/WebServiceClient.java
+++ src/dlms/service/web/client/WebServiceClient.java
@@ -1,30 +1,14 @@
 package dlms.service.web.client;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-
-import javax.xml.namespace.QName;
-import javax.xml.ws.Service;
-
 import dlms.service.web.BankServerWebInterface;
 
 public class WebServiceClient
 {
 	public static void main(String[] args)
 	{
-		URL url;
-		try
-		{
-			url = new URL("http://localhost:8888/webservice?wsdl");
+		Webservice service = new Webservice();
 
-			QName qname = new QName("http://", "webservice");
-			Service service = Service.create(url, qname);
-			BankServerWebInterface store = service
-					.getPort(BankServerWebInterface.class);
-			System.out.println(store.printCustomerInfo("TD"));
-		} catch (MalformedURLException e)
-		{
-			e.printStackTrace();
-		}
+		BankServerWebInterface store = service.getWebserviceport();
+		System.out.println(store.printCustomerInfo("TD"));
 	}
 }
